bb3a5bc991b8390f4e2648cdbba16af18fa00fe1
JuN-front <junsei1511280530@gmail.com>
2024-11-07 17:55:15 +0900

    event-newsへのスクロール導入

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const targets = document.querySelectorAll(".eventNews-container");
+
+  const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1,
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+
+  targets.forEach((target) => {
+    observer.observe(target);
+  });
+});
