699792c540053585bebdcc7ebd1e6260ed030b60
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2024-11-08 19:17:15 +0900

    カルーセル実装

--- script.js
+++ script.js
@@ -20,3 +20,21 @@ document.addEventListener("DOMContentLoaded", function () {
     observer.observe(target);
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const indicators = document.querySelectorAll(".top-image-indicator");
+
+  function updateIndicator(index) {
+      indicators.forEach((indicator, i) => {
+          indicator.classList.toggle("active", i === index);
+      });
+  }
+
+  const tl = gsap.timeline({repeat:-1});
+  tl.to(".top-image-slide", {x:"-100vw", duration:2, onStart: () => updateIndicator(1)});
+  tl.to(".top-image-slide", {x:"-200vw", duration:2, onStart: () => updateIndicator(2)});
+  tl.to(".top-image-slide", {x:"-300vw", duration:2, onStart: () => updateIndicator(3)});
+  tl.to(".top-image-slide", {x:"-400vw", duration:2, onStart: () => updateIndicator(0)});
+  tl.to(".top-image-slide", { x: 0, duration: 0});
+})
+
