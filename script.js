73ee6dae3e78fb389fb6efe7cef64fd06a885e23
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2024-11-10 10:07:43 +0900

    ダークモード実装完了

--- script.js
+++ script.js
@@ -101,7 +101,8 @@ const containers = document.querySelectorAll(
   'body, .header, .reservation-button, .features-cards, .features-cards h2, .features-textarea, .hamburger-bar, ' +
   '.hamburger, .news-font, .news-list, .news-date, .news-design, .news, .news-text, .top-image-indicator, ' +
   '.top-image-indicator.active, .rooom, .list-button-container, list-button-container:hover, .activity, .activity-button, ' +
-  '.activity-button:hover, .activity-card-description, .activity-card-text, .activity-card-title, .access-checkin' 
+  '.activity-button:hover, .activity-card-description, .activity-card-text, .activity-card-title, .access-checkin, ' +
+  '.links a, .information-container' 
 );
 
 toggle.addEventListener('change', function() {
@@ -114,17 +115,38 @@ toggle.addEventListener('change', function() {
     });
 });
 
-// ダークモード ライトモード logo変更
-const logo = document.getElementById('logo');
+// ダークモード ライトモード、ロゴ アイコン変更
+
+// アイコンとロゴの取得
+const headerlogo = document.getElementById('header-logo');
+const footerlogo = document.getElementById('footer-logo');
+const mapicon = document.getElementById('map-icon');
+const phoneicon = document.getElementById('phone-icon');
+const mailicon = document.getElementById('mail-icon');
+
+// アイコンとロゴを変更
+function updateIconsAndLogos(isDarkMode) {
+    if (isDarkMode) {
+        document.body.classList.add('dark-mode');
+        
+        // ダークモード用のロゴとアイコン
+        headerlogo.src = './images/logo-dark-mode.svg';
+        footerlogo.src = './images/footer-logo-dark-mode.svg';
+        mapicon.src = './images/map-marker-outline-dark-mode.svg';
+        phoneicon.src = './images/phone-dark-mode.svg';
+        mailicon.src = './images/email-outline-dark-mode.svg';
+    } else {
+        document.body.classList.remove('dark-mode');
+        
+        // ライトモード用のロゴとアイコン
+        headerlogo.src = './images/logo-black.svg';
+        footerlogo.src = './images/logo.svg';
+        mapicon.src = './images/map-marker-outline.png';
+        phoneicon.src = './images/phone.png';
+        mailicon.src = './images/email-outline.png';
+    }
+}
 
 toggle.addEventListener('change', function() {
-    if (this.checked) {
-        // ダークモード
-        document.body.classList.add('dark-mode');
-        logo.src = './images/logo-dark-mode.svg';  // ダークモード用のロゴに変更
-    } else {
-        // ライトモード
-        document.body.classList.remove('dark-mode');
-        logo.src = './images/logo-black.svg';  // ライトモード用のロゴに変更
-    }
+    updateIconsAndLogos(this.checked); 
 });
